23b03909e5e1437b241a95f861dfc4c5ae7c6897
Urvish Navadiya <urvishnavadiya2004@gmail.com>
2025-09-08 10:04:31 +0530

    final updation

--- src/main/java/com/exampl/GST/Bill/BillApplication.java
+++ src/main/java/com/exampl/GST/Bill/BillApplication.java
@@ -1,5 +1,6 @@
 package com.exampl.GST.Bill;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,8 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BillApplication {
 
 	public static void main(String[] args) {
-
+		Dotenv dotenv = Dotenv.load();
+		System.out.println("Loaded SID: " + dotenv.get("TWILIO_ACCOUNT_SID"));
 		SpringApplication.run(BillApplication.class, args);
 	}
-
 }
